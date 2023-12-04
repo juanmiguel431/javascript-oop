5ed8bf4c17d4f3adf5cbd7a5af13378819d3edea
juanmiguel431 <juanmiguel431@gmail.com>
2023-12-04 07:27:05 -0500

    Factory function vs Constructor function

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// Factory function
+function createUser(givenName, surname) {
+  return {
+    firstName: givenName,
+    lastName: surname,
+    fullName: function () {
+      return `${givenName} ${surname}`;
+    }
+  };
+}
+
+//Constructor function
+function User(givenName, surname) {
+  this.firstName = givenName;
+  this.lastName = surname;
+  this.fullName = function () {
+    return `${givenName} ${surname}`;
+  };
+}
+
+const user1 = createUser('Juan Miguel', 'Paulino Carpio');
+console.log(user1.firstName);
+console.log(user1.fullName());
+
+const user2 = new User('Jayleen', 'Ortega');
+console.log(user2.firstName);
+console.log(user2.fullName());
+
