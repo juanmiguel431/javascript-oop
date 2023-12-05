49820133fe742a0a38e267b9b238186835b8498d
juanmiguel431 <juanmiguel431@gmail.com>
2023-12-04 20:47:57 -0500

    Stopwatch exercise.

--- index.js
+++ index.js
@@ -62,3 +62,45 @@ const circle1 = new Circle(25);
 console.log('defaultLocation', circle1.defaultLocation);
 circle1.defaultLocation = 25;
 console.log(circle1);
+
+
+////////////////// Stopwatch
+function Stopwatch() {
+  let state = 'disabled';
+
+  let duration = 0;
+
+  let start;
+  let end;
+
+  this.start = function () {
+    if (state === 'active') {
+      throw new Error('Stopwatch is already active.');
+    }
+
+    state = 'active';
+    start = new Date();
+  };
+
+  this.stop = function () {
+    if (state === 'disabled') {
+      throw new Error('Stopwatch is already disabled.');
+    }
+
+    state = 'disabled';
+    end = new Date();
+    duration = (end - start) / 1000;
+  };
+
+  this.reset = function () {
+    duration = 0;
+    start = null;
+    end = null;
+  };
+
+  Object.defineProperty(this, 'duration', {
+    get() {
+      return duration;
+    }
+  });
+}
