f9fe1dac92217c35a810517035312641f8ee9a5b
juanmiguel431 <juanmiguel431@gmail.com>
2023-12-04 20:30:05 -0500

    getter and setter in Javascript. Object.defineProperty

--- index.js
+++ index.js
@@ -28,3 +28,37 @@ console.log(user2.firstName);
 console.log(user2.lastName);
 console.log(user2.fullName());
 
+
+function Circle(radius) {
+  this.radius = radius;
+
+  this.draw =  () => {
+    this.radius = 25;
+  }
+
+  let defaultLocation = { x: 0, y: 0 };
+
+  Object.defineProperty(this, 'defaultLocation',{
+    get: function () {
+      return defaultLocation;
+    },
+    set: function (value) {
+      if (!value.x || !value.y) {
+        throw new Error('default location is not valid.');
+      }
+      defaultLocation = value;
+    }
+  })
+}
+
+class Circle2 {
+  radius;
+  get defaultLocation() {
+    return this.radius;
+  }
+}
+
+const circle1 = new Circle(25);
+console.log('defaultLocation', circle1.defaultLocation);
+circle1.defaultLocation = 25;
+console.log(circle1);
